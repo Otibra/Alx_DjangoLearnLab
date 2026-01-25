3c104bbcef4aa38ddb74dd93a0391f568b6815b0
Otibra <otienobrian718@gmail.com>
2026-01-25 09:15:21 -0800

    views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,5 @@
-
-from django.urls import path
-from .views import list_books, LibraryDetailView, register_view, CustomLoginView, CustomLogoutView
+from django.urls import path, include
 
 urlpatterns = [
-    path('', list_books, name='home'),  # 👈 root URL
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    path('register/', register_view, name='register'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('', include('relationship_app.urls')),
 ]
