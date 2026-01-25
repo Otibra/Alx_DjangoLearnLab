67f0a741a40f32e0c473053b15810517337735bd
Otibra <otienobrian718@gmail.com>
2026-01-25 09:24:44 -0800

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,9 @@ from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import Book, Library
+from .models import Book
+from .models import Library
+
 
 # ----------------------
 # Book & Library Views
@@ -14,10 +16,12 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
 # ----------------------
 # Authentication Views
@@ -28,14 +32,16 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # logs user in after registration
+            login(request, user)
             return redirect("list_books")
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
 
+
 class CustomLoginView(LoginView):
     template_name = "relationship_app/login.html"
+
 
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
