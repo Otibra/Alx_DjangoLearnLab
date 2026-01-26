4a0bd180ed912894b4672cea2a5cd6de9fbfecb5
Otibra <otienobrian718@gmail.com>
2026-01-26 08:20:52 -0800

    views

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,15 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-
+# ----------------------
 # Author Model
+# ----------------------
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
+# ----------------------
 # Book Model
+# ----------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -17,7 +23,10 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+
+# ----------------------
 # Library Model
+# ----------------------
 class Library(models.Model):
     name = models.CharField(max_length=200)
     books = models.ManyToManyField(Book)
@@ -25,7 +34,10 @@ class Library(models.Model):
     def __str__(self):
         return self.name
 
+
+# ----------------------
 # Librarian Model
+# ----------------------
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
@@ -33,3 +45,32 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+
+# ----------------------
+# UserProfile Model
+# ----------------------
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+
+
+# ----------------------
+# Signals to auto-create UserProfile
+# ----------------------
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
