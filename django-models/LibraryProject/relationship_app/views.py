12a2523e1c2f94a3dbb7e58f2e864893a60869ce
Otibra <otienobrian718@gmail.com>
2026-01-26 08:35:54 -0800

    views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
 
 from .models import Book
 from .models import Library
@@ -45,3 +48,38 @@ class CustomLoginView(LoginView):
 
 class CustomLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+
+# ----------------------
+# Role Check Functions
+# ----------------------
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == "Admin"
+
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == "Member"
+
+
+# ----------------------
+# Role-Based Views
+# ----------------------
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
