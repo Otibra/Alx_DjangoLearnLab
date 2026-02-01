cc696d908c3d058b883f89fd7788dff7811c43e1
Otibra <otienobrian718@gmail.com>
2026-02-01 02:03:23 -0800

    new

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -2,11 +2,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
-from .models import Author
-from .models import Book
-from .models import Library
-from .models import Librarian
+# Import CustomUser from the app where it is defined (bookshelf)
+from bookshelf.models import CustomUser
+from .models import Book  # Your Book model in this app
 
 
 # ----------------------
@@ -15,33 +13,28 @@ from .models import Librarian
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     """
-    Admin configuration for the CustomUser model.
-    Includes custom fields: date_of_birth and profile_photo.
+    Custom admin for CustomUser.
+    Adds date_of_birth and profile_photo fields.
     """
 
+    # Show these fields when viewing/editing a user
     fieldsets = UserAdmin.fieldsets + (
         ("Additional Information", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
 
+    # Show these fields when creating a new user
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional Information", {
             "fields": ("date_of_birth", "profile_photo"),
         }),
     )
 
-    list_display = ("username", "email", "is_staff", "is_active")
+    # Columns to display in user list
+    list_display = ("username", "email", "is_staff", "is_active", "date_of_birth")
     search_fields = ("username", "email")
     ordering = ("username",)
-
-
-# ----------------------
-# Author Admin
-# ----------------------
-@admin.register(Author)
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ("name",)
 
 
 # ----------------------
@@ -49,23 +42,6 @@ class AuthorAdmin(admin.ModelAdmin):
 # ----------------------
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author")
-    list_filter = ("author",)
-    search_fields = ("title",)
+    list_display = ("title", "author", "publication_year")
+    search_fields = ("title", "author")
 
-
-# ----------------------
-# Library Admin
-# ----------------------
-@admin.register(Library)
-class LibraryAdmin(admin.ModelAdmin):
-    list_display = ("name",)
-    filter_horizontal = ("books",)
-
-
-# ----------------------
-# Librarian Admin
-# ----------------------
-@admin.register(Librarian)
-class LibrarianAdmin(admin.ModelAdmin):
-    list_display = ("user", "library")
--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,48 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-
-
-# ----------------------
-# Custom User Manager
-# ----------------------
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
-        if not username:
-            raise ValueError("The Username must be set")
-
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(username, password, **extra_fields)
-
-
-# ----------------------
-# Custom User Model
-# ----------------------
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(
-        upload_to="profile_photos/",
-        null=True,
-        blank=True
-    )
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # ----------------------
@@ -88,14 +47,39 @@ class Library(models.Model):
 # Librarian Model
 # ----------------------
 class Librarian(models.Model):
-    user = models.OneToOneField(
-        CustomUser,
-        on_delete=models.CASCADE
-    )
-    library = models.OneToOneField(
-        Library,
-        on_delete=models.CASCADE
-    )
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user.username
+        return self.name
+
+
+# ----------------------
+# UserProfile Model
+# ----------------------
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ("Admin", "Admin"),
+        ("Librarian", "Librarian"),
+        ("Member", "Member"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
+
+
+# ----------------------
+# Signals to auto-create UserProfile
+# ----------------------
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
