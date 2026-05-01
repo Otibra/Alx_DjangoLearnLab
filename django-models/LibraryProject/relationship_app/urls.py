c6c6c92353fd074bb632faf6d9b7ecc6cac6e9f9
Otibra <otienobrian718@gmail.com>
2026-05-01 08:05:00 -0700

    query samples

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LibraryDetailView,list_books
+from .views import list_books, LibraryDetailView
 
 urlpatterns =[
     path('books/', list_books, name='list_books'),
