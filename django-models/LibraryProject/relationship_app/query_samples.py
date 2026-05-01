a778593ac940397c99e632798e82a6ea79172ab2
Otibra <otienobrian718@gmail.com>
2026-05-01 04:32:17 -0700

    query samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -6,46 +6,48 @@ from .models import Author, Book, Librarian, Library
 # Django utility to return JSON responses
 from django.http import JsonResponse
 
+
 def list_books_in_library(library_name):
-    # Get the library by name (REQUIRED by checker)
+    # Fetch a Library object by its name (must exist or will raise DoesNotExist error)
     library = Library.objects.get(name=library_name)
 
-    # Access all books related to this library
+    # Access all Book objects related to this library via the reverse relationship
     books = library.books.all()
 
     return books
 
 
 def get_author_by_name(author_name):
-    # REQUIRED: must contain Author.objects.get(...)
+    # Retrieve a single Author object matching the given name
     author = Author.objects.get(name=author_name)
     return author
 
 
 def get_books_by_author(author):
-    # REQUIRED: must contain Book.objects.filter(author=author)
+    # Retrieve all Book objects written by the given author
     books = Book.objects.filter(author=author)
     return books
 
+
 def get_librarian_by_library(library):
-    # REQUIRED by checker: must use Librarian.objects.get(library=...)
+    # Retrieve the Librarian linked to a specific Library
     librarian = Librarian.objects.get(library=library)
     return librarian
+
 
 def query_by_auth(request):
     # Get the 'author' parameter from the URL query string (e.g. ?author=John)
     author_name = request.GET.get('author')
 
-    # If no author name is provided, return an error response
+    # If no author name is provided, return a 400 Bad Request response
     if not author_name:
         return JsonResponse({"error": "author parameter is required"}, status=400)
 
-    # Filter books whose related author name matches the provided author name
-    # NOTE: 'auther' might be a typo and should likely be 'author'
-    books = Book.objects.filter(auther__name=author_name)
+    # FIXED: correct field name should be 'author', not 'auther'
+    # Filter books whose related author's name matches the given author_name
+    books = Book.objects.filter(author__name=author_name)
 
-    # Convert queryset into a list of dictionaries so it can be returned as JSON
-    # NOTE: This line is incorrect: Book.values() should be books.values()
+    # Convert queryset into a list of dictionaries for JSON serialization
     data = list(books.values())
 
     # Return the filtered books as JSON response
@@ -53,11 +55,10 @@ def query_by_auth(request):
 
 
 def get_books_in_library(library):
-    # Returns all books related to a given library instance
-    # This assumes Library has a related name 'books' for a relationship with Book
+    # Return all books associated with the given Library instance
     return library.books.all()
 
 
 def get_librarian(library):
-    # Returns the librarian associated with the given library instance
+    # Return the Librarian linked to the given Library instance
     return library.librarian
