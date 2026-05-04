601c8281415f1b4154a8a5c3ae1c3e0d64667114
Otibra <otienobrian718@gmail.com>
2026-05-04 05:50:41 -0700

    Implement Django authentication with registration, login, logout, and protected home view

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register_view, home_view, logout_view
+from django.contrib.auth.views import LoginView
 
 urlpatterns =[
     path('books/', list_books, name='list_books'),
@@ -7,4 +8,8 @@ urlpatterns =[
     # Class-based view: shows details of a single library
     # The <int:pk> is required because DetailView expects a primary key by default
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-]
+    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('', home_view, name='home'),
+]  
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -34,4 +34,41 @@ class LibraryDetailView(DetailView):
         
         # Return the updated context dictionary
         return context
+    
+    #............register user........
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # changed here
+    else:
+        form = RegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+#........logout user........
+
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+#.......home view........
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def home_view(request):
+    return render(request, 'home.html')
 
