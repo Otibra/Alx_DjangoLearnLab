74b1eaf6465046591fe96c59886b59d2be65aa28
Otibra <otienobrian718@gmail.com>
2026-05-09 08:25:04 -0700

    Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 
 
 from .models import Book
@@ -97,7 +98,7 @@ def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
 #......add book....
-@permission_required('books.can_add_book', raise_exception=True)
+@permission_required('relationship_app.can_add_book', raise_exception=True)
 def add_book(request):
 
     if request.method == "POST":
@@ -113,7 +114,7 @@ def add_book(request):
     return render(request, 'relationship_app/add_book.html', {'form': form})
 
 #.......edit book....
-@permission_required('books.can_change_book', raise_exception=True)
+@permission_required('relationship_app.can_change_book', raise_exception=True)
 def edit_book(request, book_id):
 
     book = get_object_or_404(Book, id=book_id)
@@ -131,7 +132,7 @@ def edit_book(request, book_id):
     return render(request, 'relationship_app/edit_book.html', {'form': form})
 
 #......delete book......
-@permission_required('books.can_delete_book', raise_exception=True)
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
 def delete_book(request, book_id):
 
     book = get_object_or_404(Book, id=book_id)
