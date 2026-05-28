6f26b773b0878d588a59959320b2eef6f8588098
Otibra <otienobrian718@gmail.com>
2026-05-28 01:03:35 -0700

    Fix allowed hosts

--- social_media/social_media/urls.py
+++ social_media/social_media/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Django app is running successfully on Render!")
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/',include('account.urls')),
 ]
